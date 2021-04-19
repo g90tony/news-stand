2a46698c924803857501832fc887bc1d63853820
Caleb Tony <calebmbugua@gmail.com>
2021-04-19 13:19:50 +0300

    added get articles function

--- app/requests.py
+++ app/requests.py
@@ -8,3 +8,20 @@ def configure_request(app):
     global api_key, base_url
     api_key = app.config['API_KEY']
     base_url = app.config['BASE_URL']
+    
+def get_articles(category):
+    
+    request_url = base_url.format('top-headlines', api_key)
+    
+    with urllib.request.urlopen(request_url) as url:
+        url_response = url.read()
+        response_dict = json.loads(url_response)
+        
+        response_data = None
+        
+        if response_dict['results']:
+            processing_dict = response_data['results']            
+            response_data = processed_data(processing_dict)
+        
+    return response_data
+             
