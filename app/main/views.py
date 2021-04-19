20c79da292191383268c9952497466fc2a32bb6f
Caleb Tony <calebmbugua@gmail.com>
2021-04-19 17:12:56 +0300

    added root route to handle fetch and display sources

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,12 @@
 from flask import render_template, request, redirect, url_for
 from . import main
+from ..requests import get_articles, get_sources
 from ..models import Source, Article
 
 
 @main.route('/')
 def index():
     title = 'Home: Welcome to the News Stand'
-    return render_template('home.html', title = title)
+    sources = get_sources('sources?')
+    return render_template('sources.html', title = title, sources= sources)
+
